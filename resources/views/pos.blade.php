4071f4c7f78b86544dee2a2a976e3d439696d307
ruzjim <162730248+ruzjim@users.noreply.github.com>
2025-03-16 09:34:34 -0600

    calclulo compra JP

--- resources/views/pos.blade.php
+++ resources/views/pos.blade.php
@@ -11,122 +11,10 @@
 
     <div class="page-wrapper pos-pg-wrapper ms-0">
         <div class="content pos-design p-0">
-            <div class="btn-row d-sm-flex align-items-center">
-                <a href="javascript:void(0);" class="btn btn-secondary mb-xs-3" data-bs-toggle="modal"
-                    data-bs-target="#orders"><span class="me-1 d-flex align-items-center"><i data-feather="shopping-cart"
-                            class="feather-16"></i></span>View Orders</a>
-                <a href="javascript:void(0);" class="btn btn-info"><span class="me-1 d-flex align-items-center"><i
-                            data-feather="rotate-cw" class="feather-16"></i></span>Reset</a>
-                <a href="javascript:void(0);" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#recents"><span
-                        class="me-1 d-flex align-items-center"><i data-feather="refresh-ccw"
-                            class="feather-16"></i></span>Transaction</a>
-            </div>
-
             <div class="row align-items-start pos-wrapper">
                 <div class="col-md-12 col-lg-8">
                     <div class="pos-categories tabs_wrapper">
-                        <div class="d-none">
-                            <h5>Categoria</h5>
-                            <p>Seleccione una categoria</p>
-                            <ul class="tabs owl-carousel pos-category">
-                                <li id="all">
-                                    <a href="javascript:void(0);">
-                                        <img src="{{ URL::asset('/build/img/categories/category-01.png') }}"
-                                            alt="Categories">
-                                    </a>
-                                    <h6><a href="javascript:void(0);">All Categories</a></h6>
-                                    <span>80 Items</span>
-                                </li>
-                                <li id="headphones">
-                                    <a href="javascript:void(0);">
-                                        <img src="{{ URL::asset('/build/img/categories/category-02.png') }}"
-                                            alt="Categories">
-                                    </a>
-                                    <h6><a href="javascript:void(0);">Headphones</a></h6>
-                                    <span>4 Items</span>
-                                </li>
-                                <li id="shoes">
-                                    <a href="javascript:void(0);">
-                                        <img src="{{ URL::asset('/build/img/categories/category-03.png') }}"
-                                            alt="Categories">
-                                    </a>
-                                    <h6><a href="javascript:void(0);">Shoes</a></h6>
-                                    <span>14 Items</span>
-                                </li>
-                                <li id="mobiles">
-                                    <a href="javascript:void(0);">
-                                        <img src="{{ URL::asset('/build/img/categories/category-04.png') }}"
-                                            alt="Categories">
-                                    </a>
-                                    <h6><a href="javascript:void(0);">Mobiles</a></h6>
-                                    <span>7 Items</span>
-                                </li>
-                                <li id="watches">
-                                    <a href="javascript:void(0);">
-                                        <img src="{{ URL::asset('/build/img/categories/category-05.png') }}"
-                                            alt="Categories">
-                                    </a>
-                                    <h6><a href="javascript:void(0);">Watches</a></h6>
-                                    <span>16 Items</span>
-                                </li>
-                                <li id="laptops">
-                                    <a href="javascript:void(0);">
-                                        <img src="{{ URL::asset('/build/img/categories/category-06.png') }}"
-                                            alt="Categories">
-                                    </a>
-                                    <h6><a href="javascript:void(0);">Laptops</a></h6>
-                                    <span>18 Items</span>
-                                </li>
-                                <li id="allcategory">
-                                    <a href="javascript:void(0);">
-                                        <img src="{{ URL::asset('/build/img/categories/category-01.png') }}"
-                                            alt="Categories">
-                                    </a>
-                                    <h6><a href="javascript:void(0);">All Categories</a></h6>
-                                    <span>80 Items</span>
-                                </li>
-                                <li id="headphone">
-                                    <a href="javascript:void(0);">
-                                        <img src="{{ URL::asset('/build/img/categories/category-02.png') }}"
-                                            alt="Categories">
-                                    </a>
-                                    <h6><a href="javascript:void(0);">Headphones</a></h6>
-                                    <span>4 Items</span>
-                                </li>
-                                <li id="shoe">
-                                    <a href="javascript:void(0);">
-                                        <img src="{{ URL::asset('/build/img/categories/category-03.png') }}"
-                                            alt="Categories">
-                                    </a>
-                                    <h6><a href="javascript:void(0);">Shoes</a></h6>
-                                    <span>14 Items</span>
-                                </li>
-                                <li id="mobile">
-                                    <a href="javascript:void(0);">
-                                        <img src="{{ URL::asset('/build/img/categories/category-04.png') }}"
-                                            alt="Categories">
-                                    </a>
-                                    <h6><a href="javascript:void(0);">Mobiles</a></h6>
-                                    <span>7 Items</span>
-                                </li>
-                                <li id="watche">
-                                    <a href="javascript:void(0);">
-                                        <img src="{{ URL::asset('/build/img/categories/category-05.png') }}"
-                                            alt="Categories">
-                                    </a>
-                                    <h6><a href="javascript:void(0);">Watches</a></h6>
-                                    <span>16 Items</span>
-                                </li>
-                                <li id="laptop">
-                                    <a href="javascript:void(0);">
-                                        <img src="{{ URL::asset('/build/img/categories/category-06.png') }}"
-                                            alt="Categories">
-                                    </a>
-                                    <h6><a href="javascript:void(0);">Laptops</a></h6>
-                                    <span>18 Items</span>
-                                </li>
-                            </ul>
-                        </div>
+                        
                         <div class="pos-products">
                             <div class="d-flex align-items-center justify-content-between">
                                 <h5 class="mb-3">Productos</h5>
@@ -197,113 +85,23 @@
                             </div>
                             <div class="product-wrap" id="ListaProductos">
 
-                                {{-- <div class="product-list d-flex align-items-center justify-content-between">
-                                    <div class="d-flex align-items-center product-info" data-bs-toggle="modal"
-                                        data-bs-target="#products">
-                                        <a href="javascript:void(0);" class="img-bg">
-                                            <img src="{{ URL::asset('/build/img/products/pos-product-16.png') }}"
-                                                alt="Products">
-                                        </a>
-                                        <div class="info">
-                                            <span>PT0005</span>
-                                            <h6><a href="javascript:void(0);">Red Nike Laser</a></h6>
-                                            <p>$2000s</p>
-                                        </div>
-                                    </div>
-                                    <div class="qty-item text-center">
-                                        <a href="javascript:void(0);"
-                                            class="dec d-flex justify-content-center align-items-center"
-                                            data-bs-toggle="tooltip" data-bs-placement="top" title="minus"><i
-                                                data-feather="minus-circle" class="feather-14"></i></a>
-                                        <input type="text" class="form-control text-center" name="qty"
-                                            value="4">
-                                        <a href="javascript:void(0);"
-                                            class="inc d-flex justify-content-center align-items-center"
-                                            data-bs-toggle="tooltip" data-bs-placement="top" title="plus"><i
-                                                data-feather="plus-circle" class="feather-14"></i></a>
-                                    </div>
-                                    <div class="d-flex align-items-center action">
-                                        <a class="btn-icon edit-icon me-2" href="#" data-bs-toggle="modal"
-                                            data-bs-target="#edit-product">
-                                            <i data-feather="edit" class="feather-14"></i>
-                                        </a>
-                                        <a class="btn-icon delete-icon confirm-text" href="javascript:void(0);">
-                                            <i data-feather="trash-2" class="feather-14"></i>
-                                        </a>
-                                    </div>
-                                </div> --}}
+                                
 
 
                             </div>
                         </div>
                         <div class="block-section">
-                            <div class="selling-info">
-                                <div class="row">
-                                    <div class="col-12 col-sm-4">
-                                        <div class="input-block">
-                                            <label>Order Tax</label>
-                                            <select class="select">
-                                                <option>GST 5%</option>
-                                                <option>GST 10%</option>
-                                                <option>GST 15%</option>
-                                                <option>GST 20%</option>
-                                                <option>GST 25%</option>
-                                                <option>GST 30%</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                    <div class="col-12 col-sm-4">
-                                        <div class="input-block">
-                                            <label>Shipping</label>
-                                            <select class="select">
-                                                <option>15</option>
-                                                <option>20</option>
-                                                <option>25</option>
-                                                <option>30</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                    <div class="col-12 col-sm-4">
-                                        <div class="input-block">
-                                            <label>Discount</label>
-                                            <select class="select">
-                                                <option>10%</option>
-                                                <option>10%</option>
-                                                <option>15%</option>
-                                                <option>20%</option>
-                                                <option>25%</option>
-                                                <option>30%</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                            
                             <div class="order-total">
                                 <table class="table table-responsive table-borderless">
                                     <tr>
-                                        <td>Sub Total</td>
-                                        <td class="text-end">$60,454</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Tax (GST 5%)</td>
-                                        <td class="text-end">$40.21</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Shipping</td>
-                                        <td class="text-end">$40.21</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Sub Total</td>
-                                        <td class="text-end">$60,454</td>
-                                    </tr>
-                                    <tr>
-                                        <td class="danger">Discount (10%)</td>
-                                        <td class="danger text-end">$15.21</td>
-                                    </tr>
+                                        <td class="text-success fw-bold">Total Descuento (Usted Ahorra)</td> <!-- Juan Pa Aquí: Cambiamos a color verde y negrita -->
+                                        <td class="text-success text-end fw-bold" id="totalDescuento">-₡ 0</td> <!-- Juan Pa Aquí: Aplicamos el mismo estilo -->
+                                    </tr>                                                                        
                                     <tr>
                                         <td>Total</td>
-                                        <td class="text-end">$64,024.5</td>
-                                    </tr>
+                                        <td class="text-end" id="totalCompra">₡ 0</td>
+                                    </tr>                                    
                                 </table>
                             </div>
                         </div>
@@ -316,7 +114,7 @@
                                         <a href="javascript:void(0);">
                                             <img src="{{ URL::asset('/build/img/icons/cash-pay.svg') }}"
                                                 alt="Payment Method">
-                                            <span>Cash</span>
+                                            <span>Efectivo</span>
                                         </a>
                                     </div>
                                 </div>
@@ -325,11 +123,11 @@
                                         <a href="javascript:void(0);">
                                             <img src="{{ URL::asset('/build/img/icons/credit-card.svg') }}"
                                                 alt="Payment Method">
-                                            <span>Debit Card</span>
+                                            <span>Tarjeta</span>
                                         </a>
                                     </div>
                                 </div>
-                                <div class="col-md-6 col-lg-4 item">
+                                {{-- <div class="col-md-6 col-lg-4 item">
                                     <div class="default-cover">
                                         <a href="javascript:void(0);">
                                             <img src="{{ URL::asset('/build/img/icons/qr-scan.svg') }}"
@@ -337,28 +135,27 @@
                                             <span>Scan</span>
                                         </a>
                                     </div>
-                                </div>
+                                </div>--}}
                             </div>
                         </div>
-                        <div class="d-grid btn-block">
-                            <a class="btn btn-secondary" href="javascript:void(0);">
-                                Grand Total : $64,024.5
+                        <<div class="d-grid btn-block">
+                            <a class="btn btn-secondary" href="javascript:void(0);" id="totalPagar">
+                                Total A Pagar: ₡ 0
                             </a>
-                        </div>
+                        </div>                        
                         <div class="btn-row d-sm-flex align-items-center justify-content-between">
-                            <a href="javascript:void(0);" class="btn btn-info btn-icon flex-fill"
+                            {{-- <a href="javascript:void(0);" class="btn btn-info btn-icon flex-fill"
                                 data-bs-toggle="modal" data-bs-target="#hold-order"><span
                                     class="me-1 d-flex align-items-center"><i data-feather="pause"
-                                        class="feather-16"></i></span>Hold</a>
+                                        class="feather-16"></i></span>Hold</a>--}}
                             <a href="javascript:void(0);" class="btn btn-danger btn-icon flex-fill"><span
                                     class="me-1 d-flex align-items-center"><i data-feather="trash-2"
-                                        class="feather-16"></i></span>Void</a>
+                                        class="feather-16"></i></span>Cancelar Compra</a>
                             <a href="javascript:void(0);" class="btn btn-success btn-icon flex-fill"
                                 data-bs-toggle="modal" data-bs-target="#payment-completed"><span
                                     class="me-1 d-flex align-items-center"><i data-feather="credit-card"
-                                        class="feather-16"></i></span>Payment</a>
+                                        class="feather-16"></i></span>Proceder al Pago</a>
                         </div>
-
                     </aside>
                 </div>
             </div>
