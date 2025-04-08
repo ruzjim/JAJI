503cfffca360dee9c523c87e8a5817aa0f551d43
ruzjim <162730248+ruzjim@users.noreply.github.com>
2025-04-07 19:32:11 -0600

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\producto;
+use App\Models\Producto;
 
 class ProductController extends Controller
 {
